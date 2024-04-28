23c421a808124751f541a68c56c1f59d51676de5
saboktakin <saboktakin.hayati@gmail.com>
2024-04-28 23:24:02 +0300

    remove git icon

--- app.py
+++ app.py
@@ -82,6 +82,27 @@ import streamlit as st
 def main():
     st.set_page_config(page_title="Style Advisor: Interactive Fashion Chat")
     st.header("Fashion Assistant💁: Powered by AWS Bedrock")
+    # Add custom CSS to hide the GitHub icon
+    hide_github_icon = """
+    #GithubIcon {
+      visibility: hidden;
+    }
+    """
+    st.markdown(hide_github_icon, unsafe_allow_html=True)
+
+
+    st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
     # Adding a custom footer using markdown with unsafe_allow_html
     st.markdown("""
